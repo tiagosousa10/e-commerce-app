137a4a7fd3a9790df39705c8827c9c9d74f27ccb
tiagosousa <tiagosousa.tams@hotmail.com>
2025-02-05 19:51:35 +0100

    first styling in navbar and first steps in redux

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,10 @@ import {
   useLocation
 } from 'react-router-dom'
 import Home from './scenes/home/Home'
+import ItemDetails from './scenes/itemDetails/ItemDetails'
+import Checkout from './scenes/checkout/Checkout'
+import Confirmation from "./scenes/checkout/Confirmation";
+import Navbar from "./scenes/global/Navbar";
 
 
 const ScrollToTop = () => {
@@ -24,9 +28,14 @@ function App() {
   return (
     <div className="app">
       <BrowserRouter>
+        <Navbar/>
         <ScrollToTop />
         <Routes> 
           <Route path="/" element={<Home />} />
+          <Route path="/item/:itemId" element={<ItemDetails />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/checkout/success" element={<Confirmation />} />
+          
         </Routes>
       </BrowserRouter>
     </div>
