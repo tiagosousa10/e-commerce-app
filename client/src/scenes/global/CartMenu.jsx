88126483417327ff5b4d6af84b05a5989f9a7bcb
tiagosousa <tiagosousa.tams@hotmail.com>
2025-02-05 21:56:38 +0100

    creating the functionality for CartMenu

---
+++ client/src/scenes/global/CartMenu.jsx
@@ -0,0 +1,49 @@
+import {Box, Button, Divider, IconButton, Typography} from "@mui/material"; 
+import {useSelector, useDispatch} from 'react-redux'
+import CloseIcon from '@mui/icons-material/Close';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+import styled from "@emotion/styled";
+import {shades} from '../../theme'
+import {
+   decreaseCount,
+   increaseCount,
+   removeFromCart,
+   setIsCartOpen
+} from '../../state'
+import { useNavigate } from "react-router-dom";
+
+const FlexBox = styled(Box)`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+const CartMenu = () => {
+   const navigate = useNavigate() 
+   const dispatch = useDispatch()
+   const cart = useSelector((state) => state.cart.cart) // grabbing the cart from the state
+   const isCartOpen = useSelector((state) => state.cart.isCartOpen) // grabbing the cart from the state
+
+   const totalPrice = cart.reduce((total, item) => {
+      return total + item.count * item.attributes.price; // return the total price
+   },0)
+
+
+   return (
+      <Box
+         display={isCartOpen ? "block" : "none"}
+         backgroundColor="rgba(0, 0, 0, 0.4)"
+         position={"fixed"}
+         zIndex={10}
+         width={"100%"}
+         height={"100%"}
+         left={0}
+         top={0}
+         overflow={"auto"}
+      >
+
+      </Box>
+   )
+}
+
+export default CartMenu;
