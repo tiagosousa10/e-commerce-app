343dfd7fdac41fbb7ebc9f6146f2d0057ece59ad
tiagosousa <tiagosousa.tams@hotmail.com>
2025-02-06 20:35:18 +0100

    maincarousel done

--- client/src/scenes/home/MainCarousel.jsx
+++ client/src/scenes/home/MainCarousel.jsx
@@ -5,18 +5,16 @@ import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore"
 import NavigateNextIcon from "@mui/icons-material/NavigateNext"
 import {shades} from "../../theme"
 
-//import all images from assets folder
-const importAll = (r) => {
-  r.keys().reduce((acc,item) => {
-    acc[item.replace("./", "")] = r(item) // acc[item] = r(item)
-    return acc 
-  }, {})
-}
+// imports all images from assets folder
+const importAll = (r) =>
+  r.keys().reduce((acc, item) => {
+    acc[item.replace("./", "")] = r(item);
+    return acc;
+  }, {});
 
-const heroTextureImports = importAll( // import all images
-  require.context("../../assets", false, /\.(png|jpe?g|svg)$/) // match all image files
-)
-
+export const heroTextureImports = importAll(
+  require.context("../../assets", false, /\.(png|jpe?g|svg)$/)
+);
 
 const MainCarousel = () => {
   const isNonMobile = useMediaQuery("(min-width: 600px)") // check if screen is greater than 600px  
@@ -41,9 +39,60 @@ const MainCarousel = () => {
         >
           <NavigateBeforeIcon  sx={{fontSize: 40}} />
         </IconButton>
+      )} 
+      renderArrowNext={(onClickHandler, hasNext, label) => (
+        <IconButton
+          onClick={onClickHandler}
+          sx={{
+            position: "absolute",
+            top:"50%",
+            right: "0",
+            color: "white",
+            padding: "5px",
+            zIndex: "10",
+          }}
+        >
+          <NavigateNextIcon  sx={{fontSize: 40}} />
+        </IconButton>
       )}
     >
-
+      {Object.values(heroTextureImports).map((texture, index) => (
+        <Box key={`carousel-image-${index}`}>
+          <img 
+            src={texture}
+            alt={`carousel-${index}`}
+            style={{
+              width: "100%",
+              height: "700px",
+              objectFit: "cover",
+              backgroundAttachment: "fixed"
+            }}
+          />
+          <Box
+            color={"white"}
+            padding="20px"
+            borderRadius={"1px"}
+            textAlign={"left"}
+            bgcolor={"rgba(0, 0, 0, 0.4)"}
+            position={"absolute"}
+            top={"46%"}
+            left={isNonMobile ? "10%" : "0"}
+            right={isNonMobile ? undefined : "0"}
+            margin={isNonMobile ? undefined : "0 auto"}
+            maxWidth={isNonMobile ? undefined : "240px"}
+          >
+            <Typography color={shades.secondary[200]} variant="h5">-- NEW ITEMS</Typography>
+            <Typography variant="h1">Summer Sale</Typography>
+            <Typography 
+              fontWeight={"bold"} 
+              color={shades.secondary[300]} 
+              sx={{textDecoration: "underline"}}
+            >
+              Discover More
+            </Typography>
+          </Box>
+        </Box>
+      ))}
     </Carousel>
   )
 }
