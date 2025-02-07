65a1a085cf7eda4e17ee801902692fa6cae494d0
tiagosousa <tiagosousa.tams@hotmail.com>
2025-02-07 20:27:12 +0100

    styling itemdetails

--- client/src/scenes/itemDetails/ItemDetails.jsx
+++ client/src/scenes/itemDetails/ItemDetails.jsx
@@ -44,8 +44,64 @@ const ItemDetails = () => {
     setItems(itemsJson.data)
  }
 
+ useEffect(() => {
+  getItem()
+  getItems()
+ }, [itemId]) // eslint-disable-line react-hooks/exhaustive-deps
+
   return (
-    <div>ItemDetails</div>
+    <Box
+      width={"80%"}
+      margin={"80% auto"}
+    >
+      <Box
+        display={"flex"}
+        flexWrap={"wrap"}
+        columnGap={"40px"}
+      >
+        {/*IMAGES */}
+        <Box flex={"1 1 40%"} mb={"40px"}>
+          <img
+            alt={item?.name}
+            width={"100%"}
+            height={"100%"}
+            src={`http://localhost:1337${item?.attributes?.image?.data?.formats?.medium?.url}`}
+            style={{objectFit: "contain"}}
+          />
+        </Box>
+
+        {/*ACTIONS */}
+        <Box flex={"1 1 50%"} mb={"40px"}>
+          <Box display={"flex"} justifyContent={"space-between"}>
+            <Box>Home / Item</Box>
+            <Box>Prev Next</Box>
+          </Box>
+          
+          <Box m="65px 0 25px 0">
+            <Typography variant="h3">{item?.attributes?.title}</Typography>
+            <Typography>${item?.attributes.price}</Typography>
+            <Typography sx={{mt:"20px"}}>{item?.attributes.shortDescription}</Typography>
+          </Box>
+
+          <Box display={"flex"} alignItems={"center"} minHeight={"50px"}>
+            <Box display={"flex"} alignItems={"center"} border={`1.5px solid ${shades.neutral[300]}`} mr={"20px"} p={"2px 5px"}> 
+            <IconButton
+                        onClick={() => setCount(Math.max(count - 1, 1))}
+                     >
+                        <RemoveIcon />
+                     </IconButton>
+                     <Typography color={shades.primary[300]}>{count}</Typography>
+                     <IconButton
+                        onClick={() => setCount(Math.max(count +1))}
+                     >
+                        <AddIcon />
+                     </IconButton>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+
+    </Box>
   )
 }
 
