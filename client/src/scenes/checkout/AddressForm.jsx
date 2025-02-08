833c87c487fa6d87818940dabf82d0897a4a4770
tiagosousa <tiagosousa.tams@hotmail.com>
2025-02-08 23:44:23 +0100

    fields functions formatted

--- client/src/scenes/checkout/AddressForm.jsx
+++ client/src/scenes/checkout/AddressForm.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import {Box, useMediaQuery, TextField } from "@mui/material"
+import { getIn } from "formik"
 
-const AddressForm = () => {
+const AddressForm = ({
+   type,
+   value,
+   errors,
+   touched,
+   handleBlur,
+   handleChange,    
+}) => {
+   const isNonMobile = useMediaQuery("(min-width:600px)")
+
+   //these functions allow for better code readability
+   const formattedName = (field) => `${type}.${field}`; // type.billingAddress.street
+
+   const formattedError = (field) => 
+      Boolean(
+         getIn(touched, formattedName(field)) &&  // check if the field has been touched
+         getIn(errors, formattedName(field))
+      ); // if it has, return the error message
+
+   const formattedHelper = (field) =>
+      getIn(touched, formattedName(field)) &&  // check if the field has been touched
+      getIn(errors, formattedName(field))
+
+
   return (
-    <div>AddressForm</div>
+    <Box
+      display={"grid"}
+      gap={"15px"}
+      gridTemplateColumns="repeat(4,minmax(0,1fr))"
+      sx={{
+         "& > div" : {gridColumn: isNonMobile ? undefined : "span 4"},
+      }}
+    ></Box>
   )
 }
 
