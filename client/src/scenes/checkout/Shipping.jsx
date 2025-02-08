0d6b626501785440d0d633b9eee1c768d0bf1731
tiagosousa <tiagosousa.tams@hotmail.com>
2025-02-08 23:31:19 +0100

    adding some styles to Shipping and starting AddressForm

--- client/src/scenes/checkout/Shipping.jsx
+++ client/src/scenes/checkout/Shipping.jsx
@@ -15,7 +15,48 @@ const Shipping = ({
       m="30px auto"
     >
       {/*BILLING FORM */}
+         <Box>
+            <Typography sx={{mb:"15px"}}fontSize={"18px"}>
+               Billing Information
+            </Typography>
+            <AddressForm 
+               type="billingAddress"
+               value={values.billingAddress}
+               errors={errors}
+               touched={touched}
+               handleBlur={handleBlur}
+               handleChange={handleChange}            
+            />
+         </Box>
+         <Box mb="20px">
+            <FormControlLabel
+            label="Same for Shipping Address"
+               control={
+                  <Checkbox 
+                     defaultChecked
+                     value={values.shippingAddress.isSameAddress}
+                     onChange={(e) => setFieldValue("shippingAddress.isSameAddress", !values.shippingAddress.isSameAddress)}
+                  />
+               }
+            />
+         </Box>
 
+         {/**SHIPPING FORM **/}
+         {!values.shippingAddress.isSameAddress && (
+            <Box>
+               <Typography sx={{mb:"15px"}}fontSize={"18px"}>
+                  Shipping Information    
+               </Typography>
+               <AddressForm 
+               type="shippingAddress"
+               value={values.shippingAddress}
+               errors={errors}
+               touched={touched}
+               handleBlur={handleBlur}
+               handleChange={handleChange}            
+            />
+            </Box>
+         )}
     </Box>
   )
 }
