fb816108527b6a1ee9b300ecdaf376949ee3f60b
tiagosousa <tiagosousa.tams@hotmail.com>
2025-02-11 17:38:42 +0000

    getItems with stripe

--- server/src/api/order/controllers/order.js
+++ server/src/api/order/controllers/order.js
@@ -1,9 +1,36 @@
 'use strict';
-
+const Stripe = require("stripe");
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 /**
  * order controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::order.order');
+module.exports = createCoreController('api::order.order', ({strapi}) => ({
+  async create(ctx) {
+    const {products, userName, email} = ctx.request.body
+
+    try {
+      // retrieve item details
+      const lineItems = await Promise.all(
+        products.map(async (product) => {
+          const item = await strapi.service("api::item.item").findOne(product.id)
+
+          return {
+            price_data: {
+              currency: "usd",
+              product_data: {
+                name: item.name
+              },
+              unit_amount: item.price * 100
+            },
+            quantity: product.quantity
+          }
+        })
+      )
+    } catch(error) {
+
+    }
+  }
+}));
